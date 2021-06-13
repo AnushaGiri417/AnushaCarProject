4e4a8d394058a748611043ec15f5f789aff4b08c
AnushaGiri417 <anushagiri417@gmail.com>
2021-06-13 09:49:24 -0400

    the final commit

--- src/CarCompany/Main.java
+++ src/CarCompany/Main.java
@@ -10,8 +10,8 @@ public class Main {
         
             test cases for color of the car
             test case for immediate requirement is being written down
-                
-
+            test case to test the model of the car
+            test case to test the doors of the car
 
     }
 }
