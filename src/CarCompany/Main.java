44188292b7cf4d7c65c2af540fd0aa10b081008f
AnushaGiri417 <anushagiri417@gmail.com>
2021-06-13 09:20:52 -0400

    final red audi car ready for you

--- src/CarCompany/Main.java
+++ src/CarCompany/Main.java
@@ -3,6 +3,12 @@ package CarCompany;
 public class Main {
     public static void main(String[] args) {
         Car audi = new Car();
+            audi.number = "004816";
+            audi.model = "R8";
+            audi.color = "Red";
+            audi.door = "2";
+        
+
 
     }
 }
